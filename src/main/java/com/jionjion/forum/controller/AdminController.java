2ca015e6c2115f0ca8bfb4c77c29d58edaca9ca9
jionjion <1434501783@qq.com>
2017-12-04 23:59:10 +0800

    将前端BS3改为BS4,坑啊啊啊啊啊啊啊啊啊

--- src/main/java/com/jionjion/forum/controller/AdminController.java
+++ src/main/java/com/jionjion/forum/controller/AdminController.java
@@ -1,8 +1,9 @@
 package com.jionjion.forum.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author JionJion
@@ -11,5 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
-
+	/**
+	 * 	返回用户管理
+	 * @param model	模型
+	 * @return	 	模型页面
+	 */
+	public ModelAndView listUsers(Model model) {
+		
+		return new ModelAndView("admin/index","menuLiust",model);
+	}
 }
